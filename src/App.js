1caab1d8f4b1adb24949230f5ad4556a24c79f30
Katherine Tam <katherinemtam@gmail.com>
2021-04-26 13:42:03 -0700

    add Header & Footer files, connected to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { Component } from 'react';
-import logo from './react-logo.svg';
+import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 import React from 'react';
 
 class App extends Component {
 
   render() {
+
     return (
+
       <div className="App">
-  
-        My React App...
-        
-        <img src={logo} className="temp-images" alt="react logo" />
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
-      
+
+        <Header />
+
+        <main>
+          My React App...
+        </main>
+
+        <Footer />
+
       </div>
     );
   }
