df4cfe5947a1c71549036ec76e2377c9119fbe4d
Katherine Tam <katherinemtam@gmail.com>
2021-04-26 14:03:43 -0700

    add CreatureItem & CreatureList files

---
+++ src/CreatureItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './CreatureItem.css';
+
+class CreatureItem extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default CreatureItem;
---
+++ src/CreatureList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './CreatureList.css';
+
+class CreatureList extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default CreatureList;
